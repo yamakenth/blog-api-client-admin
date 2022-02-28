77f7e49ea12c76f5a7b8e5ab01285b8ac053295c
Ken Yamada <yamakenth@gmail.com>
2022-02-28 18:29:40 +0700

    Create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
-function ArticleFooter() {
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+import BrandLogo from './BrandLogo';
+
+function Footer() {
   return (
-    <h1>This is ArticleFooter</h1>
+    <footer className='fixed-bottom'>
+      <Navbar bg='dark' variant='dark' expand='sm'>
+        <Container>
+          <Nav>
+            <Nav.Link href='https://github.com/yamakenth/blog-api-client-admin' target='_blank'>
+              <i className='bi bi-github'>&#9;GitHub</i>
+            </Nav.Link>
+          </Nav>
+          <Navbar.Brand href='/' className='mx-0'><BrandLogo />Blog Admin</Navbar.Brand>
+          <Nav>
+            <Nav.Link href='https://google.com' target='_blank'>
+              <i className='bi bi-person'>&#9;User Page</i>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </footer>
   );
 }
 
-export default ArticleFooter;
+export default Footer;
