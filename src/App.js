f8c3c58ad7ee8d1745b0661bf637f07785f5432c
Ken Yamada <yamakenth@gmail.com>
2022-02-28 18:02:58 +0700

    Install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Button>Yeet</Button>
     </div>
   );
 }
