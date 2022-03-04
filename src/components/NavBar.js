a4175638af31740188b32deb5d024c437fb74370
Ken Yamada <yamakenth@gmail.com>
2022-03-04 15:09:47 +0700

    Replace href with React Link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 import BrandLogo from './BrandLogo';
@@ -26,7 +26,7 @@ function UserAuth() {
         <Navbar.Text className='me-3'>
           Welcome <span className='nav-username'>{username}</span>
         </Navbar.Text>
-        <Nav.Link href='/articles/create'><i className='bi bi-file-earmark-plus'>&#9;Create Article</i></Nav.Link>
+        <Nav.Link as={Link} to='/articles/create'><i className='bi bi-file-earmark-plus'>&#9;Create Article</i></Nav.Link>
         <button className='logout-button' onClick={handleClick}>
           <i className='bi bi-box-arrow-right'>&#9;Logout</i>
         </button>
@@ -35,8 +35,8 @@ function UserAuth() {
   } else {
     return (
       <Nav>
-        <Nav.Link href='/signup'><i className='bi bi-person-plus'>&#9;Signup</i></Nav.Link>
-        <Nav.Link href='/login'><i className='bi bi-box-arrow-in-right'>&#9;Login</i></Nav.Link>
+        <Nav.Link as={Link} to='/signup'><i className='bi bi-person-plus'>&#9;Signup</i></Nav.Link>
+        <Nav.Link as={Link} to='/login'><i className='bi bi-box-arrow-in-right'>&#9;Login</i></Nav.Link>
       </Nav>
     );
   }
@@ -47,7 +47,7 @@ function NavBar() {
     <header className='fixed-top'>
       <Navbar bg='dark' variant='dark' expand='sm'>
         <Container>
-          <Navbar.Brand href='/'><BrandLogo />Blog Admin</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'><BrandLogo />Blog Admin</Navbar.Brand>
           <UserAuth />
         </Container>
       </Navbar>
