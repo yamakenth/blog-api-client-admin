861ca8e0d83d62c6cc14cf4964213d30ba98184f
Ken Yamada <yamakenth@gmail.com>
2022-03-04 14:43:47 +0700

    Change api url and add helper method to slice str

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import _ from 'underscore';
 import moment from 'moment';
 import { Card, Row, Col } from 'react-bootstrap';
 
+// helper method to slice strings
+function sliceStr(str, len) {
+  const oldWordCnt = str.split(' ').length;
+  
+  let shortStrArr = str.split(' ').slice(0, len);
+  const newWordCnt = shortStrArr.length;
+
+  let slicedStr = shortStrArr.join(' ');
+  if (oldWordCnt > newWordCnt) {
+    slicedStr += '...';
+  }
+  
+  return slicedStr;
+}
+
 function ArticleList() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:1000/blog/articles')
-      .then((res) => {
+    axios.get('https://yamakenth-blog-api-server.herokuapp.com/api/articles')
+      .then(res => {
         setArticles(res.data);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
@@ -26,26 +42,26 @@ function ArticleList() {
             .map((article) => {
               return (
                 <Col sm={6} md={4} xl={3} className='mb-3 d-flex align-items-stretch' key={article._id}>
-                  <Card className='w-100'>
+                  <Card className='w-100 article-card'>
                     <Card.Body 
-                      className={`d-flex flex-column justify-content-between ${(article.published) ? '' : 'unpublish-article'} article-card-body`}
+                      className={`d-flex flex-column justify-content-between ${(article.published) ? '' : 'unpublish-article'}`}
                     >
                       <div className='mb-3'>
-                        <Card.Title>{_.unescape(article.title)}</Card.Title>
+                        <Card.Title>{sliceStr(_.unescape(article.title), 10)}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">
                           {_.unescape(article.author.username)}&nbsp;
                           |&nbsp;&nbsp;{moment(article.createdAt).format('MMMM Do YYYY')}
                         </Card.Subtitle>
                         <Card.Text>
-                          {_.unescape(article.text).split(' ').slice(0, 20).join(' ') + '...'}
+                          {sliceStr(_.unescape(article.text), 20)}
                         </Card.Text>
                       </div>
                       <div className='d-flex justify-content-between'>
                         {localStorage.getItem('username') &&
-                          <Card.Link href={`/articles/${article._id}/edit`}>Edit</Card.Link>
+                          <Card.Link as={Link} to={`/articles/${article._id}/edit`}>Edit</Card.Link>
                         }
                         {!localStorage.getItem('username') &&
-                          <Card.Link href={`/login`}>Login to edit</Card.Link>
+                          <Card.Link as={Link} to={`/login`}>Login to edit</Card.Link>
                         }
                         <Card.Text>Published: {(article.published) ? 'Yes' : 'No'}</Card.Text>
                       </div>
